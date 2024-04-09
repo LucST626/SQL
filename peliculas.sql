49b894acc38716b7c14465b5e99c06cfd82e505b
LucST626 <146832827+LucST626@users.noreply.github.com>
2024-04-09 13:52:17 +0200

    add

--- peliculas.sql
+++ peliculas.sql
@@ -28,6 +28,12 @@ INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario2', 'No mires arriba'
 INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario3', 'The Batman');
 INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario4', 'Dune');
 INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario5', 'Top Gun: Maverick');
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario6', 'Top Gun: Maverick');
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario7', 'Top Gun: Maverick');
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario8', 'Top Gun: Maverick');
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario9', 'Top Gun: Maverick');
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario10', 'Top Gun: Maverick');
+
 
 
 INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Dune', 'Ciencia Ficción', 2021, 'Denis Villeneuve', 1);
@@ -35,3 +41,6 @@ INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('No mires 
 INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('The Batman', 'Acción', 2022, 'Matt Reeves', 3);
 INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Top Gun: Maverick', 'Acción', 2022, 'Joseph Kosinski', 4);
 INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Elvis', 'Biopic', 1994, 'Baz Luhrmann', 5);
+INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('asd', 'Biopic', 1994, 'Baz Luhrmann', 5);
+INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Elvis', 'Biopic', 1994, 'Baz Luhrmann', 5);
+INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Elgtvis', 'Biopic', 1994, 'Baz Luhrmann', 5);
